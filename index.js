958a66e61dc66ae9ac81976c52c83ab6a0e653a0
mirsamie <mirsamie@PROGRAMMER11>
2020-12-12 15:38:34 +0330

    .

--- index.js
+++ index.js
@@ -21,34 +21,38 @@ const client = new Client(new TDLib(process.env.TDLIB_PATH), {
 });
 
 async function main() {
-  const configsObject = new ConfigsClass();
-  const configs = await configsObject.getConfigs();
-  
-  if(!configs.enable_main_bot){
-    process.exit();
-  }
-
-  client
-  .on('update', update => {
-    if(update.chat_id && (update.chat_id==configs.firstGroupChatId || update.chat_id==configs.secondGroupChatId || update.chat_id==configs.thirdGroupChatId)  && (update._=="updateChatLastMessage" || update._=="updateDeleteMessages")){
-      if(update._=="updateChatLastMessage"){
-        const msg = update.last_message.content.text.text;
-        const msgObject = new MessageClass(redisClient, msg, update.last_message.sender_user_id, update.last_message.id, update.chat_id);
-        if(msgObject.status)
-          Logic.findPrices(redisClient, client, configs).then().catch(err => console.log('PRICES Error:', err))  
-
-      }else {
-        MessageClass.deleteMessages(redisClient, update.message_ids)
-      }
+  try{
+    const configsObject = new ConfigsClass();
+    const configs = await configsObject.getConfigs();
+    
+    if(!configs.enable_main_bot){
+      process.exit();
     }
-  })
-  .on('error', err => {
-    console.error('Got error:', JSON.stringify(err, null, 2))
-  })
-  .on('destroy', () => {
-    console.log('destroy event')
-  })
-  await client.connectAndLogin();
+
+    client
+    .on('update', update => {
+      if(update.chat_id && (update.chat_id==configs.firstGroupChatId || update.chat_id==configs.secondGroupChatId || update.chat_id==configs.thirdGroupChatId)  && (update._=="updateChatLastMessage" || update._=="updateDeleteMessages")){
+        if(update._=="updateChatLastMessage"){
+          const msg = update.last_message.content.text.text;
+          const msgObject = new MessageClass(redisClient, msg, update.last_message.sender_user_id, update.last_message.id, update.chat_id);
+          if(msgObject.status)
+            Logic.findPrices(redisClient, client, configs).then().catch(err => console.log('PRICES Error:', err))  
+
+        }else {
+          MessageClass.deleteMessages(redisClient, update.message_ids)
+        }
+      }
+    })
+    .on('error', err => {
+      console.error('Got error:', JSON.stringify(err, null, 2))
+    })
+    .on('destroy', () => {
+      console.log('destroy event')
+    })
+    await client.connectAndLogin();
+  }catch(e){
+    console.log(e)
+  }
 }
 
 main();
